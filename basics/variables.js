2d0481f0d6e7fc9d6d9b829e9657800f720fb2cb
Skanda Prasad <160585876+skanda-prasaad@users.noreply.github.com>
2024-02-20 13:33:44 +0000

    var and dt

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 85858585
+let accountEmail="skanda@gmail.com"
+var accountPassword="858585758"
+accountCity="Bengaluru"
+
+// accountId = 858555 not allowed , no changes to be done in cosnt.
+accountEmail="skakakkka@gmail.com"
+accountPassword="58444444"
+accountCity="mangalore"
+let accountStates;
+
+console.log(accountId);
+
+
+/*
+Prefer not to use var bcz of issue im block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountStates])

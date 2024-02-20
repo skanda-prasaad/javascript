dfe131756addb08144c57df6177f128cb0c518b5
Skanda Prasad <160585876+skanda-prasaad@users.noreply.github.com>
2024-02-20 14:19:19 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,58 @@
+// let score = "33"
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+
+
+// let score = "66abc"
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); 
+
+// let score = null
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); 
+
+// let score = undefined
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); 
+
+// let score = true
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); 
+
+let score = "Skanda"
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber); 
+
+// ******* note :- ******* 
+
+// "33" => 33
+// "33abc" => value is NaN (not a number) , but type is number
+// true => 1
+// false => 0 
+
+let isLoggedIn = 0
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanLoggedIn);
+
+// ****** note :- *********
+
+// 1 => true ; 0 => flase 
+// "" => false
+// "skanda" => true
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))

365f615988e7a20d0d8c273c47be6ca61a30578c
Skanda Prasad <160585876+skanda-prasaad@users.noreply.github.com>
2024-02-20 13:54:12 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node  js not browser 
+
+let name="skanda"
+let age= 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null =>standalone value
+//undefined => variable is written nut value are not provided
+//symbol => unique
+
+// object
+
+console.log(typeof"skanda");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

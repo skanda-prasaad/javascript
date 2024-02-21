767926c258e9d2fe934dee539263014d973e33cb
Skanda Prasad <160585876+skanda-prasaad@users.noreply.github.com>
2024-02-21 07:50:17 +0000

    datatypes-summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// ***** Primitive 
+
+// 7 types :- strings , Number , boolean , null , undefined , Symbol , BigInt
+
+const score = 100 
+const isLoggedIn = true (boolean)
+const outsideTemp = null (null)
+let userEmail;
+
+
+// ***** Reference ( non Primitive) :-
+
+Array , Objects , Functions
+
+
+const heroes =["spiderman", "batman"] (Array);
+
+let myObj = {
+    name:"skanda",
+    age: 18,
+}
+
+let myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof (score));

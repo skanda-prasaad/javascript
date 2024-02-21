a54996f0be4be18cbf15358037107c1eb81dab59
Skanda Prasad <160585876+skanda-prasaad@users.noreply.github.com>
2024-02-21 07:24:01 +0000

    operation

--- basics/operation.js
+++ basics/operation.js
@@ -1,1 +1,23 @@
-let value
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 =" Skanda";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 * 2) + 5);
+
+// prefix to postfix 

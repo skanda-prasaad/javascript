bfe79d609656de967817740f5ba834aef5ae34ac
Skanda Prasad <160585876+skanda-prasaad@users.noreply.github.com>
2024-02-23 06:57:01 +0000

    math and num

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,36 @@
+
+
+// *************  Numebers ****************//
+
+const score = 100 
+//console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const newNumber = 45888888.26;
+//console.log(newNumber.toLocaleString());
+
+const otherNum = 585.235;
+//console.log(otherNum.toPrecision(4));
+
+
+// *********** Maths ************* //
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.8));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(4,8,8,1,3,0));
+
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);

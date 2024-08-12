d618de33c0eab805866f98b364c53165924761ad
Skanda Prasad <skandaprasad595@gmail.com>
2024-08-12 18:23:52 +0530

    Started

---
+++ variables_datatypes/console/index.js
@@ -0,0 +1,7 @@
+console.log("hello");
+console.error("Alert");
+console.warn("Warning");
+console.table({firstname:'Yooo', email: 'too@gmail.com'});
+// can able to add css styles to console.
+const styles = 'padding:10px;background-color:yellow; color:red';
+console.log('%cHello World',styles);
---
+++ variables_datatypes/variables/variables.js
@@ -0,0 +1,13 @@
+// Ways to declare a variable
+// var,const,let
+
+let firstName = "John";
+let lastName = "alex";
+console.log(firstName, lastName, age);
+var age = 30;
+// we can't log 'let' before initilizing but we can in using 'var'(undefined).
+ 
+// using push() to add in array
+const arr = [1,2,6,2];
+arr.push(8);
+console.log(arr);

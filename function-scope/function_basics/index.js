99eb2e6ba277cf5721283fa970a722a46a7e045e
Skanda Prasad <skandaprasad595@gmail.com>
2024-08-13 14:43:44 +0530

    first-commit

---
+++ function-scope/function_basics/index.js
@@ -0,0 +1,17 @@
+function Hello(){
+    console.log("Hello World");
+}
+Hello();
+
+function add(x,y){
+    console.log(x+y);
+} //  x and y are parameters.
+
+add(1,6); // 1 and 6 are arguments
+
+function sub (x , y){
+    return x-y;
+}
+
+const result = sub(7,3);
+console.log(result);

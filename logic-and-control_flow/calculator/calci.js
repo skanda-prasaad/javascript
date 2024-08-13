bc08c2905cf6046c481727d0c4e7c6fa4998f74d
Skanda Prasad <skandaprasad595@gmail.com>
2024-08-13 18:38:57 +0530

    calci

---
+++ logic-and-control_flow/calculator/calci.js
@@ -0,0 +1,16 @@
+
+  // add and sub calci
+
+function calculator(a,b,exp){
+    switch(exp){
+        case '+' :
+            return a+b;
+            break;
+        case '-' :
+            return a-b;
+            break;
+    }
+}
+
+console.log(calculator(5,3,'+'));
+

eea217c24911e6a42f765ac6e0197b8f5da937fc
Skanda Prasad <skandaprasad595@gmail.com>
2024-09-06 22:03:09 +0530

    promise

--- promises/script.js
+++ promises/script.js
@@ -1,5 +1,19 @@
-import fs from 'fs';
+const randomNum = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        const random = Math.random();
+        if(random > 0.5){
+            resolve(random);
+        }
+        else{
+            reject("value is too samll");
+        }
+    }, 2000);
+})
 
-function SkandasReadFile(){
-    
-}
+randomNum
+.then(function(result){
+    console.log("promise is fulfilled, the value is : " , result);
+})
+.catch(function(error){
+    console.log("promise is rejected");
+})
